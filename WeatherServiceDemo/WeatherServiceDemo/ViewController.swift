6be0830072f6c471871d4bd5a2734089cceac399
Jayesh Kawli <j.kawli@gmail.com>
2023-12-09 19:41:37 +0530

    Added initial structure to the repo

---
+++ WeatherServiceDemo/WeatherServiceDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherServiceDemo
+//
+//  Created by Jayesh Kawli on 12/9/23.
+//
+
+import WeatherService
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
